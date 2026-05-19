f103143fe9d118831ae6d4a3e63da8fe220fde99
AlexTec1488 <Shchegolev1-ao@mail.ru>
2026-05-19 18:53:36 +0300

    Add files via upload

---
+++ DzPolimorphism/Company.java
@@ -0,0 +1,28 @@
+package DzPolimorphism;
+
+public class Company {
+    private final String title;
+    private int debit;
+    private int credit;
+    private final TaxSystem taxSystem;
+
+    public Company(String title, TaxSystem taxSystem) {
+        this.title = title;
+        this.taxSystem = taxSystem;
+    }
+
+
+    public void shiftMoney(int amount) {
+        if (amount > 0) {
+            this.debit += amount;
+        }
+        if (amount < 0){
+            this.credit += Math.abs(amount);
+        }
+    }
+
+    public void payTaxes() {
+        System.out.println("Компания " + title + " уплатила налог в размере: " + taxSystem.calcTaxFor(debit, credit) + " руб.");
+        debit = credit = 0;
+    }
+}
