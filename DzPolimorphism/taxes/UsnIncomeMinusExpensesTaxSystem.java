dc03f8a8a9c11917f2910354be5d1285541ea846
AlexTec1488 <Shchegolev1-ao@mail.ru>
2026-05-19 20:05:52 +0300

    Add files via upload

---
+++ DzPolimorphism/taxes/UsnIncomeMinusExpensesTaxSystem.java
@@ -0,0 +1,10 @@
+package DzPolimorphism.taxes;
+
+public class UsnIncomeMinusExpensesTaxSystem extends TaxSystem {
+    @Override
+    public int calcTaxFor(int debit, int credit) {
+        int inCome = debit - credit;
+        int tax = inCome * 15 / 100;
+        return Math.max(tax, 0);
+    }
+}
