44c56d5da008d690d1f4b9d594f0d0de3b405150
AlexTec1488 <Shchegolev1-ao@mail.ru>
2026-05-25 03:18:41 +0300

    Add files via upload

---
+++ DzAbstraction/Account.java
@@ -0,0 +1,20 @@
+package DzAbstraction;
+
+public abstract class Account {
+    protected long balance;
+
+    public Account(long balance) {
+        this.balance = balance;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public abstract boolean add(long amount);
+
+    public abstract boolean pay(long amount);
+
+    public abstract boolean transfer(Account account, long amount);
+
+}
---
+++ DzAbstraction/Main.java
@@ -0,0 +1,8 @@
+package DzAbstraction;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
